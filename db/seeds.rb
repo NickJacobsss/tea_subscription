2384e0f9e4c678fbd557cd5b0f2617360a05dd4c
Nick Jacobs <nickjacobsatc@gmail.com>
2022-09-20 11:55:56 -0500

    Seeds: Created 2 customers, teas, and subs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Subscription.destroy_all
+Customer.destroy_all
+Tea.destroy_all
+
+nick = Customer.create(first_name: "nick", last_name: "jacobs", email: "nickjacobs@gmail.com", address: "123 ABC Street")
+titan = Customer.create(first_name: "titan", last_name: "jacobs", email: "IamACat@gmail.com", address: "456 DEF Street")
+
+black = Tea.create(title: "Black Tea", description: "Dark Leaves, highly caffinated", temp: "300F", brew_time: "5 Minutes")
+green = Tea.create(title: "Green Tea", description: "Light Leaves, high antioxidant content", temp: "350F", brew_time: "10 Minutes")
+
+Subscription.create(title: "Black Tea Once", price: 10.15, frequency: 0, customer_id: nick.id, tea_id: black.id)
+Subscription.create(title: "Green Tea Weekly", price: 5.75, frequency: 1, customer_id: titan.id, tea_id: green.id)
